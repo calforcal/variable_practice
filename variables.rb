712598296f00c8d620ab67e707489fc770cd2331
Michael Callahan <mcalla242@gmail.com>
2023-02-22 11:49:52 -0700

    Fix comments with correct syntax

--- variables.rb
+++ variables.rb
@@ -1,27 +1,28 @@
-// Strings
+#Strings
 name = "Michael"
 city = "Boulder"
 bike_brand = "Canyon"
 
-// Integers
+#Integers
 age = 27
 zip_code = 80304
 bike_count = 2
 
-// Floats
+#Floats
 miles_biked = 14382.44
 account_balance = 1054.86
 
-//Booleans
+#Booleans
 likes_bikes = true
 enrolled = false
 currently_baking = false
 
-//Reassigning Strings
+#Reassigning Strings
 name = "Carolyn"
 city = "Lansdale"
 bike_brand = "Specialized"
 
+# Puts Statements
 puts "My name is #{name}."
 puts "I live in the city of #{city}."
 puts "I ride a #{bike_brand} bicycle."
